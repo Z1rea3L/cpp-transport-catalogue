ed4d5d03514f55ac4c57fefe2fd28c98b80e320f
Nikita <137328449+Z1rea3L@users.noreply.github.com>
2024-04-16 13:03:09 +0300

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -1,1 +1,38 @@
-// место для вашего кода
+#include "stat_reader.h"
+
+void ParseAndPrintStat(const transport_catalogue::TransportCatalogue& transport_catalogue, std::string_view request, std::ostream& output) {
+    
+    std::string_view parsed_request = request.substr(0, request.find(' '));
+    std::string_view request_data = request.substr(request.find(' ')+1,request.size());
+    
+    //output<<"|"<<parsed_request<<"|"<<" {"<<request_data<<"} ";
+    
+    if(parsed_request == "Bus"){
+        auto bus = transport_catalogue.FindBus(request_data);
+        if(bus==nullptr){
+            output<<request<<": not found"<<std::endl;
+        }else{
+            output<<request<<": "<<bus->stops.size()
+                   <<" stops on route, "<< bus->unique_stops.size()
+                   <<" unique stops, " << transport_catalogue.GetBusDistance(bus)
+                   <<" route length"<<std::endl;
+        }
+    }
+    
+    if(parsed_request == "Stop"){
+        auto stop = transport_catalogue.FindStop(request_data);
+        if(stop==nullptr){
+            output<<request<<": not found"<<std::endl;
+            
+        }else if(transport_catalogue.GetBusesOfStop(stop).size()==0){
+            output<<request<<": no buses"<<std::endl;
+            
+        }else{
+            output<<request<<": buses ";
+            for(const std::string_view bus: transport_catalogue.GetBusesOfStop(stop)){
+                output<<bus<<" ";
+            }
+            output<<std::endl;
+        }
+    }
+}
