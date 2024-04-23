70650390695de8c6339a294cb2edd675fb36a8e8
Nikita <137328449+Z1rea3L@users.noreply.github.com>
2024-04-23 14:17:41 +0300

    Update transport_catalogue.cpp

--- transport-catalogue/transport_catalogue.cpp
+++ transport-catalogue/transport_catalogue.cpp
@@ -11,6 +11,7 @@ namespace transport_catalogue
 
         stopname_to_stop_.insert({all_stops_.back().name, &all_stops_.back()});
         stop_to_busname_[&all_stops_.back()]={};
+        
     }
 
     const Stop* TransportCatalogue::FindStop(std::string_view stop_name)const{
@@ -46,17 +47,32 @@ namespace transport_catalogue
         }
         return nullptr;
     }
-
-    double TransportCatalogue::GetBusDistance(const Bus* bus)const{
-        double result = 0.;
-        const int counter = (bus->stops.size())-1;
-        for(int i = 0; i< counter; ++i){
-            result+=geo::ComputeDistance(bus->stops[i]->coordinates,bus->stops[i+1]->coordinates);
-        }
-        return result;
-    }
-
+    
     std::set<std::string_view> TransportCatalogue::GetBusesOfStop(const Stop* stop)const{
         return stop_to_busname_.at(stop);
+    }
+    
+    void TransportCatalogue::SetStopsDistance(const Stop* stop_lhs, const Stop* stop_rhs, const int distance){
+        stops_distances_[{stop_lhs,stop_rhs}]=distance;
+    }
+    
+    int TransportCatalogue::GetStopsDistance(const Stop* stop_lhs, const Stop* stop_rhs)const{
+        if(stops_distances_.contains({stop_lhs,stop_rhs})){
+            return stops_distances_.at({stop_lhs,stop_rhs});
+        }else if (stops_distances_.contains({stop_rhs,stop_lhs})){
+            return stops_distances_.at({stop_rhs,stop_lhs});
+        }
+        return 0;
+    }
+    
+    std::pair<int,double> TransportCatalogue::GetBusDistAndCurvature(const Bus* bus)const{
+        double dist_by_coordinates = 0.;
+        int distance_by_stops = 0;
+        const int counter = (bus->stops.size())-1;
+        for(int i = 0; i< counter; ++i){
+            dist_by_coordinates += geo::ComputeDistance(bus->stops[i]->coordinates,bus->stops[i+1]->coordinates);
+            distance_by_stops += GetStopsDistance(bus->stops[i],bus->stops[i+1]);
+        }
+        return {distance_by_stops,distance_by_stops/dist_by_coordinates};
     }
 }
